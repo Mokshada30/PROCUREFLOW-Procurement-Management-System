9606245b4579643611bdce490742c72549c55a8e
Mokshada Mali <144531335+Mokshada30@users.noreply.github.com>
2025-08-21 07:52:38 +0530

    Update supabase.js
    temp changes.

--- procurement-frontend/utils/supabase.js
+++ procurement-frontend/utils/supabase.js
@@ -2,5 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("Supabase Anon Key:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 10) + "...");
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
